d1d8a40f47ef6d6772773600d9434d92e8b49307
github username <github email address>
2018-08-02 17:03:19 -0400

    Added semantic ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :type_of_animals
-  resources :users 
+  resources :users
   resources :animals
   resources :shelters
+
+  get '/', :to => 'animals#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
