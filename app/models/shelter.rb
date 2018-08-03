2ddfe6f53fe58f45bd60c69b734157ea5cb1f547
Josh <joshua.ross.miller@gmail.com>
2018-08-03 15:17:24 -0400

    added validations

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,6 +1,17 @@
 class Shelter < ApplicationRecord
-  has_many :animals
+  has_many :animals, dependent: :destroy
 
-  validates :name, presence: true
-  validates :address, presence: true
+  validates :name,:address,:mission,:email,:phone, presence: true
+
+  validate :email_checker
+  validates :phone, length: {is: 7}
+
+  def email_checker
+    email = self.email.split("")
+    if !email.include?('@')
+      errors.add(:email, message:"email in inproper format")
+    end
+  end
+
+
 end
